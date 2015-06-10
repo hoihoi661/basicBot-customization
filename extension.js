8e40cf19de2edd8b8a69f202ccc0c8b7d4fae81d
Matthijs Hatzmann <matthijs.hatzmann@hotmail.com>
2015-06-10 21:18:47 +0200

    Pushtest2
    Last one didn't return anything at !bacon?

--- extension.js
+++ extension.js
@@ -43,7 +43,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Bacon :dansgame:");
+                    API.sendChat("/me Bacon!!!!!");
                 }
             }
         };
